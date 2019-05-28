5ab90b288f23226f10c266a4f4ec406442b550df
eshwar00001 <51072032+eshwar00001@users.noreply.github.com>
2019-05-28 12:27:17 +0530

    Update views.py

--- HelloWorld/views.py
+++ HelloWorld/views.py
@@ -4,6 +4,9 @@ def hello(request):
    print("hello")
    print("HII")
    print("welcome to hello world")
+   import cx_Oracle
+   conn = cx_Oracle.connect(user=r'User Name', password='Personal Password')
+   c = conn.cursor()
    text = """<h1>welcome to my app <br> HelloWorld !</h1>"""
    print("No HII")
 
