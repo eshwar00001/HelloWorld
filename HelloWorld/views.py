dfdb6ce0c8ed67ccc781918f76eadf07104383d0
eshwar00001 <51072032+eshwar00001@users.noreply.github.com>
2019-05-28 10:25:48 +0530

    Update views.py

--- HelloWorld/views.py
+++ HelloWorld/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 
 def hello(request):
+   print("hello")
+   print("HII")
+   print("welcome to hello world")
    text = """<h1>welcome to my app <br> HelloWorld !</h1>"""
+   print("No HII")
 
    return HttpResponse(text)
